9f83fe05af565d3cdebbc3b739c5bd1c2caf7085
GiauNGUYEN12 <giaunguyen0216@gmail.com>
2024-11-26 15:05:54 +0100

    Mettre les ingrédients dans les buttons

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const buttonContainer = document.getElementById("buttonContainer")
+
+let listeIngredients = ["carrots", "tomatoes", "steaks", "spagettis", "potatoes", "fish", "butter", "egg york"]
+listeIngredients.forEach(ingredient => {
+    const optionIngredient = document.createElement('button')
+    optionIngredient.innerHTML = `<p> ${ingredient} </p>`
+    optionIngredient.addEventListener("click", () => {
+        console.log(optionIngredient.innerText)
+    })
+    buttonContainer.appendChild(optionIngredient)
+})
 async function displayRecipes (){
     const reponse = await fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=carrots,tomatoes&number=10&limitLicense=true&ranking=1&ignorePantry=false&apiKey=04aeff9c7aa54db4add1b4bf65923c49")
     const result = await reponse.json()
