94aa33dd7038fb2fe3af133636e609fa0230fbfa
GiauNGUYEN12 <giaunguyen0216@gmail.com>
2024-12-02 09:57:13 +0100

    Afficher les images ingrédients dans les buttons

--- script.js
+++ script.js
@@ -5,19 +5,32 @@ const buttonNouvelleRecette = document.getElementById("nouvelleRecette")
 const buttonRetour = document.getElementById("buttonRetour")
 let selectedIngredient = new Set()
 
-let listeIngredients = ["carrots", "tomatoes", "steaks", "spagettis","potatoes", 
-    "fish", "butter", "egg york","broccoli","olives","flour","cucumber"]
-listeIngredients.forEach(ingredient => {
+let listeIngredients = {
+    ingredients: [
+{text:"carrots", image:"images/carrotes3.png"},
+{text:"tomatoes", image:"images/tomatoes.png"},
+{text:"steaks", image:"images/steak.png"},
+{text:"spagettis", image:"images/spagettis.png"},
+{text:"potatoes", image:"images/potatoes.png"},
+{text:"fish", image:"images/fish.png"},
+{text:"butter", image:"images/butter.png"},
+{text:"egg york", image:"images/egg-york.png"},
+{text:"broccoli", image:"images/brocoli.png"},
+{text:"olives", image:"images/olives.png"},
+{text:"flour", image:"images/flour.png"},
+{text:"cucumber", image:"images/cucumber.png"}
+]}
+listeIngredients.ingredients.forEach(ingredient => {
     const optionIngredient = document.createElement('button')
-    optionIngredient.innerHTML = `<p> ${ingredient} </p>`
+    optionIngredient.innerHTML = `<img src = "${ingredient.image}" class= "ingredients"/>`
     buttonContainer.appendChild(optionIngredient)
 
     optionIngredient.addEventListener("click", () => {
         //console.log(optionIngredient.innerText)
-        if (selectedIngredient.has(ingredient)) {
-            selectedIngredient.delete(ingredient)
+        if (selectedIngredient.has(ingredient.text)) {
+            selectedIngredient.delete(ingredient.text)
         } else {
-            selectedIngredient.add(ingredient)
+            selectedIngredient.add(ingredient.text)
         }
         console.log(Array.from(selectedIngredient))
     }) 
@@ -28,8 +41,7 @@ async function displayRecipes (url,index){
     recipesResult = await recipesReponse.json()
     //console.log(recipesResult)
     afficheNomDeRecette.innerHTML=""
-    //recipesResult.forEach(ingredient => {
-    //let indexDeTrois = recipesResult.length/4
+
     for (let i = 0; i < index; i++){
         const recipeElement = document.createElement('div')
         recipeElement.innerHTML =
@@ -40,40 +52,34 @@ async function displayRecipes (url,index){
     afficheNomDeRecette.appendChild(recipeElement)
     } 
     }
-
 buttonValider.addEventListener("click",() => {
     // on masque le première page 
     document.getElementById("ingredientsPage").style.display ="none"
-    document.getElementById("recipesPage").style.display="flex"
+    document.getElementById("recipesPage").style.display="inline-block"
 
     const selectedArray = Array.from(selectedIngredient); // Convertit le Set en tableau
-    //console.log("Ingrédients choisis :", selectedArray)
+    console.log("Ingrédients choisis :", selectedArray)
     ingredientsGroupe = selectedArray.map((ingredient) => encodeURIComponent(ingredient)).join(",")
-    //console.log("ingredient regroupée:",ingredientsGroupe)
-    //console.log(remplaceIngredient(ingredientsGroupe))
+    console.log("ingredient regroupée:",ingredientsGroupe)
     displayRecipes(remplaceIngredient(ingredientsGroupe),3) 
     document.getElementById('texte').innerText = "Voici les plats possibles avec vos ingrédients"
-    //console.log(recipesAleatoire(recipesResult))
 })
 let nombreRecetteIndex = 3
 buttonNouvelleRecette.addEventListener("click", ()=>{
-    nombreRecetteIndex+=3;
-    // console.log(nombreRecette, "ceci est un test")
-    if(nombreRecetteIndex<recipesResult.length){
-    displayRecipes(remplaceIngredient(ingredientsGroupe),nombreRecetteIndex)
+    nombreRecetteIndex += 3
+    if (nombreRecetteIndex < recipesResult.length){
+        displayRecipes(remplaceIngredient(ingredientsGroupe),nombreRecetteIndex)
     }else if(nombreRecetteIndex = recipesResult.length){
-    buttonNouvelleRecette.style.display="none"
-    buttonRetour.style.display = "inline-block"
-    displayRecipes(remplaceIngredient(ingredientsGroupe),nombreRecetteIndex)
-    }else{
+        buttonNouvelleRecette.style.display = "none"
+        buttonRetour.style.display = "inline-block"
+        displayRecipes(remplaceIngredient(ingredientsGroupe),nombreRecetteIndex)
+    } else {
     }
 })
 buttonRetour.addEventListener("click", () => {
-    document.getElementById("recipesPage").style.display="none"
     document.getElementById("ingredientsPage").style.display ="inline-block"
+    document.getElementById("recipesPage").style.display="none"
 })
-
 function remplaceIngredient(ingredientsChoisi){
-    return  `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsChoisi}&number=12&limitLicense=true&ranking=1&ignorePantry=false&apiKey=7061bca4946b450486a78ee6c6b7e398`
+    return  `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsChoisi}&number=12&limitLicense=true&ranking=1&ignorePantry=false&apiKey=04aeff9c7aa54db4add1b4bf65923c49`
 }
-//console.log(recipesAleatoire(recipesResult))
