01aac3c2815c1ee9f512db0dedf57450b5ea9fc6
GiauNGUYEN12 <giaunguyen0216@gmail.com>
2024-12-04 11:11:36 +0100

    Afficher les ingrédients dans le frigo

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const buttonContainer = document.getElementById("buttonContainer")
+const buttonContainer2 = document.getElementById("buttonContainer2")
+const buttonContainer3 = document.getElementById("buttonContainer3")
 const buttonCook = document.getElementById("buttonValider")
-const afficheNomDeRecette = document.getElementById("nomDeRecette")
+const recetteContainer = document.getElementById("recetteContainer")
 const buttonNouvelleRecette = document.getElementById("nouvelleRecette")
 const buttonRetour = document.getElementById("buttonRetour")
 const fridgeOpenningToggle = document.getElementById("frigo")
+const loader = document.getElementById("loading")
 const apiKey = "2d65b549176d4a439deba01412837ab9"
-const recipePerPage = 3
+const recipePerPage = 6
 let selectedIngredient = new Set()
 let currentRecipes = new Array()
 
@@ -25,14 +28,17 @@ let listeIngredients = {
 {text:"cucumber", image:"images/cucumber.png"}
 ]}
 buttonCook.disabled = true
-listeIngredients.ingredients.forEach(ingredient => {
+let buttonContainers = [buttonContainer, buttonContainer2, buttonContainer3]
+listeIngredients.ingredients.forEach((ingredient, index) => {
+    let container = buttonContainers[index % 3]
     const optionIngredient = document.createElement('button')
-    optionIngredient.innerHTML = `<img src = "${ingredient.image}" class= "ingredients"/>`
-    buttonContainer.appendChild(optionIngredient)
-
+    optionIngredient.innerHTML = `<img src = "${ingredient.image}" class= "${ingredient.text}"/>`
+    container.appendChild(optionIngredient)
+        //ingredient.classList.add("carrots","tomatoes","steaks","spagettis","potatoes","fish","butter","egg york","broccoli","olives","flour","cucumber")
+    
     optionIngredient.addEventListener("click", () => {
         buttonCook.disabled = false
-        //console.log(optionIngredient.innerText)
+            //console.log(optionIngredient.innerText)
         if (selectedIngredient.has(ingredient.text)) {
             selectedIngredient.delete(ingredient.text)
         } else {
@@ -40,32 +46,40 @@ listeIngredients.ingredients.forEach(ingredient => {
         }
         console.log(Array.from(selectedIngredient))
     }) 
+    
 })
 buttonCook.addEventListener("click",() => {
     // on masque le première page 
-    document.getElementById("ingredientsPage").style.display ="none"
+    document.getElementById("ingredientsPage").style.display = "none"
     document.getElementById("recipesPage").style.display="inline-block"
 
     const selectedArray = Array.from(selectedIngredient); // Convertit le Set en tableau
-    console.log("Ingrédients choisis :", selectedArray)
+    //console.log("Ingrédients choisis :", selectedArray)
     ingredientsGroupe = selectedArray.map((ingredient) => encodeURIComponent(ingredient)).join(",")
-    console.log("ingredient regroupée:",ingredientsGroupe)
-    
-    //currentRecipes = remplaceIngredient(ingredientsGroupe)
-    // displayRecipes(remplaceIngredient(ingredientsGroupe),0) 
+    //console.log("ingredient regroupée:",ingredientsGroupe)
     document.getElementById('texte').innerText = "Voici les plats possibles avec vos ingrédients"
     loadRecipes(ingredientsGroupe)
+})
+buttonContainers.forEach(container => {
+    container.style.display ="none"
 })
 let isFridgeOpen = false
 fridgeOpenningToggle.addEventListener('click',() => {
     if(isFridgeOpen){
         fridgeOpenningToggle.src = "images/frigo-fermé.png"
-        fridgeOpenningToggle.style= "width: 220px"
+        fridgeOpenningToggle.style= "width: 500px"
         isFridgeOpen = false
+        buttonContainers.forEach(container => {
+            container.style.display ="none"
+        })
+        
     } else {
-        fridgeOpenningToggle.src = "images/frigo-ouvert.png";
-        fridgeOpenningToggle.style= "width: 400px"
+        fridgeOpenningToggle.src = "images/frigo-ouvert3.png";
+        fridgeOpenningToggle.style= "width: 870px"
         isFridgeOpen = true
+        buttonContainers.forEach(container => {
+            container.style.display ="inline-block"
+        })
   }
 })
 let recipePageIndex = 0
@@ -92,19 +106,20 @@ buttonRetour.addEventListener("click", () => {
 })
 
 async function loadRecipes(ingredientsChoisi) {
+    loader.style.display = "inline-block"
     let url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsChoisi}&number=12&limitLicense=true&ranking=1&ignorePantry=false&apiKey=${apiKey}`
     const recipesReponse = await fetch(url)
     currentRecipes = await recipesReponse.json()
+    loader.style.display = "none"
     console.log(currentRecipes)
     recipePageIndex = 0
 
-    afficheNomDeRecette.innerHTML=""
+    recetteContainer.innerHTML=""
     console.log(currentRecipes)
     displayRecipes()
 }
-
 function displayRecipes() {
-    afficheNomDeRecette.innerHTML = ""
+    recetteContainer.innerHTML = ""
     console.log(recipePageIndex)
     for (let i = recipePageIndex * recipePerPage; (i < currentRecipes.length) && (i < (recipePageIndex + 1) * recipePerPage); i++){
         const recipeElement = document.createElement('button')
@@ -112,7 +127,7 @@ function displayRecipes() {
         `<p>${currentRecipes[i].title}</p> 
         <img src = "${currentRecipes[i].image}"/>
         `
-        afficheNomDeRecette.appendChild(recipeElement)
+        recetteContainer.appendChild(recipeElement)
         recipeElement.classList.add("recipeElementContainer")
     } 
 }
